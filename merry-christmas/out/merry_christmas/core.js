dcfae8170a5f3c8575a68210342961ed09e9dffe
Minsun Lee <dalzony@gmail.com>
2016-12-20 21:55:04 +0900

    text 추가

--- merry-christmas/out/merry_christmas/core.js
+++ merry-christmas/out/merry_christmas/core.js
@@ -9,16 +9,18 @@ merry_christmas.core.load_image = (function merry_christmas$core$load_image(imag
 var path = [cljs.core.str("./resources/"),cljs.core.str(cljs.core.name.call(null,image)),cljs.core.str(".png")].join('');
 return quil.core.request_image.call(null,path);
 });
-merry_christmas.core.draw_image = (function merry_christmas$core$draw_image(state,k,p__9059){
-var vec__9063 = p__9059;
-var x = cljs.core.nth.call(null,vec__9063,(0),null);
-var y = cljs.core.nth.call(null,vec__9063,(1),null);
+merry_christmas.core.draw_image = (function merry_christmas$core$draw_image(state,k,p__8651){
+var vec__8655 = p__8651;
+var x = cljs.core.nth.call(null,vec__8655,(0),null);
+var y = cljs.core.nth.call(null,vec__8655,(1),null);
 return quil.core.image.call(null,cljs.core.get_in.call(null,state,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"images","images",1757475080),k], null)),x,y);
 });
 merry_christmas.core.setup = (function merry_christmas$core$setup(){
 quil.core.frame_rate.call(null,(30));
 
 quil.core.color_mode.call(null,new cljs.core.Keyword(null,"rgb","rgb",1432123467));
+
+quil.core.text_font.call(null,quil.core.create_font.call(null,"DejaVu Sans",(28),true));
 
 quil.core.image_mode.call(null,new cljs.core.Keyword(null,"center","center",-748944368));
 
@@ -27,10 +29,10 @@ return new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"im
 merry_christmas.core.draw_state = (function merry_christmas$core$draw_state(state){
 var w = quil.core.width.call(null);
 var h = quil.core.height.call(null);
-var tr__8120__auto__ = new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(w / (2)),(h / (2))], null);
+var tr__8120__auto___8658 = new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(w / (2)),(h / (2))], null);
 quil.core.push_matrix.call(null);
 
-try{quil.core.translate.call(null,tr__8120__auto__);
+try{quil.core.translate.call(null,tr__8120__auto___8658);
 
 quil.core.scale.call(null,((function (){var x__6881__auto__ = w;
 var y__6882__auto__ = h;
@@ -39,59 +41,61 @@ return ((x__6881__auto__ < y__6882__auto__) ? x__6881__auto__ : y__6882__auto__)
 
 quil.core.background.call(null,(89),(117),(80));
 
-var tr__8120__auto___9066__$1 = new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(0),(0)], null);
+var tr__8120__auto___8659__$1 = new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(0),(0)], null);
 quil.core.push_matrix.call(null);
 
-try{quil.core.translate.call(null,tr__8120__auto___9066__$1);
+try{quil.core.translate.call(null,tr__8120__auto___8659__$1);
 
 merry_christmas.core.draw_image.call(null,state,new cljs.core.Keyword(null,"santa","santa",-599568745),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(0),(0)], null));
 }finally {quil.core.pop_matrix.call(null);
+}}finally {quil.core.pop_matrix.call(null);
 }
-quil.core.pop_matrix.call(null);
+quil.core.stroke_weight.call(null,(20));
 
-return quil.core.fill.call(null,(255),((255) * new cljs.core.Keyword(null,"fade","fade",1167694157).cljs$core$IFn$_invoke$arity$1(state)));
-}finally {quil.core.pop_matrix.call(null);
-}});
+quil.core.text.call(null,"Merry Christmas~!",(140),(100));
+
+return quil.core.text.call(null,"@dalzony",(180),(480));
+});
 merry_christmas.core.merry_christmas = (function merry_christmas$core$merry_christmas(){
 return quil.sketch.sketch.call(null,new cljs.core.Keyword(null,"host","host",-1558485167),"merry-christmas",new cljs.core.Keyword(null,"size","size",1098693007),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(500),(500)], null),new cljs.core.Keyword(null,"setup","setup",1987730512),((cljs.core.fn_QMARK_.call(null,merry_christmas.core.setup))?(function() { 
-var G__9067__delegate = function (args){
+var G__8660__delegate = function (args){
 return cljs.core.apply.call(null,merry_christmas.core.setup,args);
 };
-var G__9067 = function (var_args){
+var G__8660 = function (var_args){
 var args = null;
 if (arguments.length > 0) {
-var G__9068__i = 0, G__9068__a = new Array(arguments.length -  0);
-while (G__9068__i < G__9068__a.length) {G__9068__a[G__9068__i] = arguments[G__9068__i + 0]; ++G__9068__i;}
-  args = new cljs.core.IndexedSeq(G__9068__a,0);
+var G__8661__i = 0, G__8661__a = new Array(arguments.length -  0);
+while (G__8661__i < G__8661__a.length) {G__8661__a[G__8661__i] = arguments[G__8661__i + 0]; ++G__8661__i;}
+  args = new cljs.core.IndexedSeq(G__8661__a,0);
 } 
-return G__9067__delegate.call(this,args);};
-G__9067.cljs$lang$maxFixedArity = 0;
-G__9067.cljs$lang$applyTo = (function (arglist__9069){
-var args = cljs.core.seq(arglist__9069);
-return G__9067__delegate(args);
+return G__8660__delegate.call(this,args);};
+G__8660.cljs$lang$maxFixedArity = 0;
+G__8660.cljs$lang$applyTo = (function (arglist__8662){
+var args = cljs.core.seq(arglist__8662);
+return G__8660__delegate(args);
 });
-G__9067.cljs$core$IFn$_invoke$arity$variadic = G__9067__delegate;
-return G__9067;
+G__8660.cljs$core$IFn$_invoke$arity$variadic = G__8660__delegate;
+return G__8660;
 })()
 :merry_christmas.core.setup),new cljs.core.Keyword(null,"middleware","middleware",1462115504),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [quil.middleware.fun_mode], null),new cljs.core.Keyword(null,"draw","draw",1358331674),((cljs.core.fn_QMARK_.call(null,merry_christmas.core.draw_state))?(function() { 
-var G__9070__delegate = function (args){
+var G__8663__delegate = function (args){
 return cljs.core.apply.call(null,merry_christmas.core.draw_state,args);
 };
-var G__9070 = function (var_args){
+var G__8663 = function (var_args){
 var args = null;
 if (arguments.length > 0) {
-var G__9071__i = 0, G__9071__a = new Array(arguments.length -  0);
-while (G__9071__i < G__9071__a.length) {G__9071__a[G__9071__i] = arguments[G__9071__i + 0]; ++G__9071__i;}
-  args = new cljs.core.IndexedSeq(G__9071__a,0);
+var G__8664__i = 0, G__8664__a = new Array(arguments.length -  0);
+while (G__8664__i < G__8664__a.length) {G__8664__a[G__8664__i] = arguments[G__8664__i + 0]; ++G__8664__i;}
+  args = new cljs.core.IndexedSeq(G__8664__a,0);
 } 
-return G__9070__delegate.call(this,args);};
-G__9070.cljs$lang$maxFixedArity = 0;
-G__9070.cljs$lang$applyTo = (function (arglist__9072){
-var args = cljs.core.seq(arglist__9072);
-return G__9070__delegate(args);
+return G__8663__delegate.call(this,args);};
+G__8663.cljs$lang$maxFixedArity = 0;
+G__8663.cljs$lang$applyTo = (function (arglist__8665){
+var args = cljs.core.seq(arglist__8665);
+return G__8663__delegate(args);
 });
-G__9070.cljs$core$IFn$_invoke$arity$variadic = G__9070__delegate;
-return G__9070;
+G__8663.cljs$core$IFn$_invoke$arity$variadic = G__8663__delegate;
+return G__8663;
 })()
 :merry_christmas.core.draw_state));
 });
--- merry-christmas/src/merry_christmas/core.cljs
+++ merry-christmas/src/merry_christmas/core.cljs
@@ -15,21 +15,27 @@
 (defn setup []
   (q/frame-rate 30)
   (q/color-mode :rgb)
+  (q/text-font (q/create-font "DejaVu Sans" 28 true))
   (q/image-mode :center)
   {:images (zipmap images (map load-image images))})
 
 (defn draw-state [state]
   (let [w (q/width)
         h (q/height)]
+
     (q/with-translation [(/ w 2) (/ h 2)]
       (q/scale (/ (min w h) (* 2 image-size)))
       (q/background 89 117 80)
 
       (q/with-translation [0 0]
-        (draw-image state :santa [0 0]))
-      (q/pop-matrix)
+        (draw-image state :santa [0 0])))
+    (q/stroke-weight 20)
 
-      (q/fill 255 (* 255 (:fade state))))))
+    ;; `fill` sets the fill color (text color, and color inside shapes).
+    (q/text "Merry Christmas~!" 140 100)
+    (q/text "@dalzony" 180 480))
+
+  )
 
 (q/defsketch merry-christmas
   :host "merry-christmas"
